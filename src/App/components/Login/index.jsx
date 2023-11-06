86898cf5ad1680d38fcf4098221dc42ce3ca9857
sindiga-dev <is.sindiga@gmail.com>
2023-11-06 10:13:58 +0300

    Authentication

--- src/App/components/Login/index.jsx
+++ src/App/components/Login/index.jsx
@@ -8,34 +8,73 @@ function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [notify, setNotify] = useState(false);
+  //form validation
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
 
   function notifyUser() {
     setNotify((notify) => !notify);
     setTimeout(endNotification, 1000);
   }
-
-  function submitHandler(e) {
-    setError(null)
-    e.preventDefault();
-    fetch("http://127.0.0.1:3000/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email, password }),
-    }).then((res) => {
-      if (res.ok) {
-        res.json().then((user) => {
-            notifyUser();
-          localStorage.setItem("jwt", user.jwt);
-          localStorage.setItem("passenger", `${user.passenger.id}`);
-        });
-      } else {
-        res.json().then((error) => setError(error));
-      }
-    });
+  //  set the corresponding error state variables.
+  function emailIsValid(email) {
+  // Regular expression for basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
   }
 
+  function passwordIsValid(password) {
+    // Password must be at least 6 characters long
+    return password.length >= 6;
+  }
+  function handleEmailChange(e) {
+    setEmail(e.target.value);
+    setEmailError("");
+  }
+
+  function handlePasswordChange(e) {
+    setPassword(e.target.value);
+    setPasswordError("");
+  }
+
+
+  function submitHandler(e) {
+  e.preventDefault();
+
+  // Validation logic for email and password
+  if (!emailIsValid(email)) {
+    setEmailError("Invalid email address");
+    return;
+  }
+
+  if (!passwordIsValid(password)) {
+    setPasswordError("Password must be at least 6 characters long");
+    return;
+  }
+
+  setError(null);
+
+  // Making the API call with validated data
+  fetch("http://127.0.0.1:3000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+  .then((res) => {
+    if (res.ok) {
+      res.json().then((user) => {
+        notifyUser();
+        localStorage.setItem("jwt", user.jwt);
+        localStorage.setItem("passenger", `${user.passenger.id}`);
+      });
+    } else {
+      res.json().then((error) => setError(error));
+    }
+  });
+}
   function endNotification() {
     setNotify((notify) => !notify);
     navigate("/dashboard");
@@ -93,7 +132,11 @@ function Login() {
           <label>Password</label>
         </div>
         <button className="outline text-sky-600 hover:bg-sky-600 hover:text-white text-2xl px-5 place-self-center rounded-lg h-10" type="submit">Login</button>
-
+          <div>
+            
+          </div><span></span>
+  <label>Password</label>
+  <p className="error-message">{passwordError}</p>
         <div className="signup_link">
           Not a member?
           <NavLink to="/signup">Signup</NavLink>
